d564d2dcd853cd8eee554d336f1a8448f7b1934f
bbeizer <89622436+bbeizer@users.noreply.github.com>
2022-03-23 14:26:00 -0400

    changes

--- pa02/transactions.py
+++ pa02/transactions.py
@@ -1,3 +1,14 @@
+import sqlite3
+
+def to_cat_dict(transaction_tuple):
+    ''' cat is a category tuple (rowid, name, desc)'''
+    transaction = {'item #':transaction_tuple[0], 'amount':transaction_tuple[1], 'category':transaction_tuple[2], 'date':transaction_tuple[3], 'description': transaction_tuple[4]}
+    return cat
+
+def to_cat_dict_list(cat_tuples):
+    ''' convert a list of category tuples into a list of dictionaries'''
+    return [to_cat_dict(cat) for cat in cat_tuples]
+
 class Transaction:
 
     def __init__(self, dbfile):
@@ -10,10 +21,10 @@ class Transaction:
         self.dbfile = dbfile
 
     def select_all(self):
-        ''' return all of the categories as a list of dicts.'''
+        ''' return all of the transactions as a list of dicts.'''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute("SELECT rowid,* from categories")
+        cur.execute("SELECT rowid,* from ")
         tuples = cur.fetchall()
         con.commit()
         con.close()
